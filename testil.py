dfb3e13035caee3c09394dffde0de7ee44ebc69f
imp96 <ilyam19@gmail.com>
2017-05-26 18:05:08 +0000

    Classes addded

--- testil.py
+++ testil.py
@@ -5,11 +5,12 @@ from generation import *
 weights = matrix_from_file("w_5_1.txt")
 bias = flip_bias(list_from_file("lf_5_1.txt"))
 
+states = [0 for i in range(25)]
+#ic = Simulation_Noise(states, weights, bias, 10000, 0.07))
+print_test_results(*repetitive_test(Simulation_Basic, 5, 2, weights, bias, 1000000))
 
-print_test_results(*repetitive_test(run_simulation_basic, 10, 2, weights, bias, 1000000))
 
+print_test_results (*repetitive_test(Simulation_Noise, 5, 2, weights, bias, 1000000, 0.01))
 
-print_test_results (*repetitive_test(run_simulation_theirs, 10, 2, weights, bias, 1000000, 0.01))
-
-print_test_results (*repetitive_test(run_simulation_flip, 10, 2, weights, bias, 1000000, 10,7,2))
+#print_test_results (*repetitive_test(run_simulation_flip, 10, 2, weights, bias, 1000000, 10,7,2))
 
